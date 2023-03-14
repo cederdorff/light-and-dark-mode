ce929262f1e586fb5018ac3b781a39ed6971f27e
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-03-14 13:48:08 +0100

    green and yellow mode added

--- app.js
+++ app.js
@@ -10,11 +10,21 @@ function initApp() {
 function modeChanged() {
     const mode = this.value;
     console.log(mode);
+    resetMode(); // reset no matter what
+
     if (mode === "dark") {
         console.log("Dark Mode");
         document.body.classList.add("dark-mode");
-    } else {
-        console.log("Might be light mode");
-        document.body.classList.remove("dark-mode");
+    } else if (mode === "green") {
+        console.log("Green Mode");
+        document.body.classList.add("green-mode");
+    } else if (mode === "yellow") {
+        console.log("Yellow Mode");
+        document.body.classList.add("yellow-mode");
     }
+}
+
+// removes all *-mode classes from body
+function resetMode() {
+    document.body.classList.remove("dark-mode", "green-mode", "yellow-mode");
 }
